dbd7d477eff1d6c9ff8bb60ae325563202ed889e
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-07 12:04:05 -0800

    [feat] pass search api response data to new state

--- client/app/directives/searchDirectives.js
+++ client/app/directives/searchDirectives.js
@@ -1,14 +1,16 @@
-angular.module('tinnr.searchDirectives', ['tinnr.recipes', ])
-  .directive('searchbar', ['Recipes', function (Recipes) {
+angular.module('tinnr.searchDirectives', ['tinnr.recipes', 'ui.router'])
+  .directive('searchbar', ['Recipes', '$state', function (Recipes, $state) {
     return {
       restrict: 'E',
       templateUrl: 'app/partials/_search.html',
       controller: function ($scope) {
         $scope.preferences = {};
+        $scope.searchResponse;
         $scope.getRecipes = function () {
           Recipes.getRecipes($scope.preferences)
             .then(function (data) {
-
+              $scope.searchResponse = data;
+              $state.go('recipeList', { searchResponse: $scope.searchResponse });
             })
             .catch(function (error) {
               console.error('Error fetching recipes: ', error);
