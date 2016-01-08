4d7689c65bb4200ecb7be816b52fdcc2ddc6f3f4
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-07 17:32:08 -0800

    [refactor] add api logic to recipeList controller in order to allow page refresh and back button in browser

--- client/app/recipeList/recipeList.js
+++ client/app/recipeList/recipeList.js
@@ -1,14 +1,30 @@
 angular.module('tinnr.recipeList', ['ui.router'])
-  .controller('RecipeListController', ['$scope', '$stateParams', '$state', function ($scope, $stateParams, $state) {
+  .controller('RecipeListController', ['$scope', '$stateParams', '$state', 'Recipes', function ($scope, $stateParams, $state, Recipes) {
     $scope.cols = 4;
     $scope.meals = [];
     $scope.offsets = 0;
+    $scope.preferences = {};
+    $scope.preferences.q = $stateParams.searchQuery;
 
-    console.log('this is $scope.meals in the recipeList.js file', JSON.parse($stateParams.searchResponse).matches);
-    $scope.searchResults = JSON.parse($stateParams.searchResponse).matches;
-    $scope.offsets = $scope.cols - ($scope.searchResults.length % $scope.cols);
-    $scope.meals = _.chunk($scope.searchResults, $scope.cols);
+    $scope.getRecipes = function (params) {
+      console.log('the get recipes function is happening');
+      Recipes.getRecipes(params)
+        .then(function (data) {
+          console.log('this got executed', JSON.parse(data).matches);
+          $scope.meals = JSON.parse(data).matches;
+          $scope.offsets = $scope.cols - ($scope.meals.length % $scope.cols);
+          $scope.meals = _.chunk($scope.meals, $scope.cols);
+        })
+        .catch(function (error) {
+          console.error('Error fetching recipes: ', error);
+        });
+    };
+
+    $scope.getRecipes($scope.preferences);
+
+    console.log('this is $stateParams in the recipeList.js file', $stateParams.searchQuery);
+
     $scope.getFormulaPage = function (id) {
       $state.go('formula', { id: id });
-    }
+    };
   }]);
